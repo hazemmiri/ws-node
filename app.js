4fac0a27a0623c4571b09a99fac8caa6e0ec045f
hazemmiri <hazemmiri1989@gmail.com>
2024-01-08 02:54:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("this is app.js file");
+// import file system
+const fs = require("fs");
+//import notes.js
+const notes = requier("./notes")
+// import yargs
+const yargs = require("yargs");
+console.log("Process ARVG : " , process.argv);
+console.log("yargs ARVG : " , yargs.argv);
---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log("this is notes.js file")
+
+const add = (a,b)=>{
+    return a+b
+}
+function minus(a,b){
+    return a-b
+}
+
+module.exports = {add}
